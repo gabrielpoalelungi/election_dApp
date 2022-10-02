5e72ecf127ee1bc0a8a21f6bc7e5c3f2a7f92915
Gabriel Poalelungi <gabriel1.poalelungi@gmail.com>
2022-10-02 15:03:28 +0300

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    // Constructor
+    // Store candidate
+    // Read candidate
+
+    string public candidate;
+
+    constructor()  public {
+        candidate = "Candidate1";
+    }
+}
