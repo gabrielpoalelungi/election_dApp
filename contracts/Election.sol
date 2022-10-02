cf3d2eb670f18cea9e70c8af6a6cd349ea726912
Gabriel Poalelungi <gabriel1.poalelungi@gmail.com>
2022-10-02 16:28:10 +0300

    Second commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,28 @@
 pragma solidity ^0.5.0;
 
 contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
     // Constructor
-    // Store candidate
-    // Read candidate
-
-    string public candidate;
-
     constructor()  public {
-        candidate = "Candidate1";
+        addCandidate("Candidate1");
+        addCandidate("Candidate2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
